523c0a23ef4f9e6da66b5e81be0460eea91513b0
Encogt <143376535+Encogt@users.noreply.github.com>
2024-10-17 15:39:03 +0800

    Add files via upload
    Added Diagnosis, Inventory, ReplenishmentRequest, Treatment class.

---
+++ SC2002_Project/src/hospitalManagementSystem/Treatment.java
@@ -0,0 +1,12 @@
+package hospitalManagementSystem;
+import java.util.Date;
+
+public class Treatment {
+
+    private String treatmentID;
+
+    private String description;
+
+    private Date localDate;
+
+}
